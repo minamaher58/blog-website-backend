067f44f45773404e5163bfcac0679839f7eb1d10
minamaher58 <53014650+minamaher58@users.noreply.github.com>
2021-02-11 08:38:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useFind
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join("images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", '*');
